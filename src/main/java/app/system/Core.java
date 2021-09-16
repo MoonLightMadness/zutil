dc98977bbf72da976b16eb6c7d4fd1fea135caf6
zhl <2377459837@qq.com>
2021-09-16 09:19:09 +0800

    线程池更新

--- src/main/java/app/system/Core.java
+++ src/main/java/app/system/Core.java
@@ -1,7 +1,14 @@
 package app.system;
 
+import app.config.Config;
+import app.config.impl.NormalConfig;
+import app.log.Log;
+import app.log.impl.NormalLog;
+
 public class Core {
 
+    public static Log log = new NormalLog();
 
+    public static Config configer = new NormalConfig();
 
 }
