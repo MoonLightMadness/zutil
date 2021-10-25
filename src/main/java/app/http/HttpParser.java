bd538d5482009102e7286c6166b6b557c0b53f80
Deep_Dark_Fantasy <39981781+MoonLightMadness@users.noreply.github.com>
2021-10-25 19:05:44 +0800

    优化代码

--- src/main/java/app/http/HttpParser.java
+++ src/main/java/app/http/HttpParser.java
@@ -13,33 +13,51 @@ import app.parser.JSONTool;
  */
 public class HttpParser {
 
-    public static HttpRequestEntity parseRequestEntiy(byte[] data){
+    public static HttpRequestEntity parseRequestEntiy(byte[] data) {
         String sdata = new String(data);
         HttpRequestEntity httpRequestEntity = new HttpRequestEntity();
-        String method = sdata.substring(0,sdata.indexOf(" "));
-        if(!"POST".equals(method)){
+        String method = sdata.substring(0, sdata.indexOf(" "));
+        if (!"POST".equals(method) && !"GET".equals(method)) {
             return null;
         }
         httpRequestEntity.setMethod(method);
-        httpRequestEntity.setArgs(sdata.substring(sdata.indexOf(" ")+1,sdata.indexOf(" ",httpRequestEntity.getMethod().length()+1)));
+        String args = sdata.substring(sdata.indexOf(" ") + 1, sdata.indexOf(" ", httpRequestEntity.getMethod().length() + 1));
+        httpRequestEntity.setArgs(args.split("\\?")[0]);
+        String[] subArgs = new String[0];
+        if (args.indexOf('?') != -1) {
+            subArgs = args.split("\\?")[1].split("&");
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("{\n");
+        for (String sa : subArgs) {
+            builder.append("\t\"").append(sa.split("=")[0]).append("\":");
+            builder.append("\"").append(sa.split("=")[1]).append("\",\n");
+        }
+        if (sdata.indexOf('{') != -1) {
+            builder.append(sdata, sdata.indexOf("{") + 1, sdata.lastIndexOf("}") + 1);
+        } else {
+            builder = builder.deleteCharAt(builder.length() - 2);
+            builder.append("\n}");
+        }
         //存在无数据的情况
         try {
-            httpRequestEntity.setBody(sdata.substring(sdata.indexOf("{"),sdata.lastIndexOf("}")+1));
-        }catch (Exception e) {
+            System.out.println(builder.toString());
+            httpRequestEntity.setBody(builder.toString());
+        } catch (Exception e) {
             //e.printStackTrace();
         }
         return httpRequestEntity;
     }
 
-    public static HttpRespondEntity parseRespondEntity(byte[] data){
+    public static HttpRespondEntity parseRespondEntity(byte[] data) {
         String sdata = new String(data);
         HttpRespondEntity httpRespondEntity = new HttpRespondEntity();
-        httpRespondEntity.setProtocol(sdata.substring(0,sdata.indexOf(" ")));
-        httpRespondEntity.setBody(sdata.substring(sdata.indexOf("{"),sdata.lastIndexOf("}")+1));
+        httpRespondEntity.setProtocol(sdata.substring(0, sdata.indexOf(" ")));
+        httpRespondEntity.setBody(sdata.substring(sdata.indexOf("{"), sdata.lastIndexOf("}") + 1));
         return httpRespondEntity;
     }
 
-    public static String constructRequest(HttpRequestEntity httpRequestEntity){
+    public static String constructRequest(HttpRequestEntity httpRequestEntity) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(httpRequestEntity.getMethod());
         stringBuilder.append(" HTTP/1.1\n");
